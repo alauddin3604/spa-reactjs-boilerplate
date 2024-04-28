a8d2d9a0d6e66f4ab95d72363250fba6d3769c67
Alauddin <alauddin3604@gmail.com>
2024-04-28 19:13:43 +0800

    Refactor

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,33 +1,55 @@
+import BaseTable from '@/components/BaseTable'
 import SpinnerOverlay from '@/components/SpinnerOverlay'
-import User from '@/types/User'
 import UserService from '@/services/UserService'
+import User from '@/types/User'
 import { useEffect, useState } from 'react'
+import { Button, Card, Modal } from 'react-bootstrap'
 
 const UserIndex = () => {
+    const [showModal, setShowModal] = useState(false)
     const [users, setUsers] = useState(Array<User>)
     const [isFetching, setIsFetching] = useState(false)
 
     useEffect(() => {
         setIsFetching(true)
-        UserService.index().then(({ data: { data } }) => {
+        UserService.index({}).then(({ data: { data } }) => {
             setUsers(data)
         })
             .finally(() => setIsFetching(false))
     }, [])
 
+    const openModal = () => setShowModal(true)
+    const closeModal = () => setShowModal(false)
+
     return (
-        <div className="card">
-            <div className="card-body">
-                <ol>
-                    {users.map((user: User) => (
-                        <li key={user.id}>
-                            {user.name} - {user.email} {/* Render whatever user information you want */}
-                        </li>
-                    ))}
-                </ol>
-            </div>
+        <Card>
+            <Card.Header>
+                <h3 className="card-title">Users</h3>
+            </Card.Header>
+            <Card.Body>
+                <div className="d-flex mb-3">
+                    <div className="ms-auto">
+                        <Button variant="primary" onClick={openModal}>Add New User</Button>
+                    </div>
+                </div>
+                <BaseTable data={users} />
+            </Card.Body>
             <SpinnerOverlay isShowing={isFetching} />
-        </div>
+            <Modal centered={true} show={showModal} onHide={closeModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={closeModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={closeModal}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </Card>
     )
 }
 
